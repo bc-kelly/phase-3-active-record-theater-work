f820f2cbed6e7b3c6f7d976ee5e38a128da76a8b
Bridget Kelly <bckelly197@gmail.com>
2022-03-21 11:35:44 -0400

    got tables

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,9 @@
 class Audition < ActiveRecord::Base
-  
+
+    def change 
+       
+    end 
+    
 end
+
+##bunlde exec rake db:create_migration NAME = create_auditions
---
+++ app/models/role.rb
@@ -0,0 +1,12 @@
+class Role  < ActiveRecord::Base
+
+    @@actor = []
+    @@location = []
+
+    # def change
+    #     create_table :roles do |t|
+    #         t.string :character_name
+    #     end
+    # end 
+
+end 
